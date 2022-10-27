84676e43eaea320e43f652133806a60287421f91
Dvd Elbaz <dvdelbaz@mbp-de-dvd.home>
2022-10-27 17:32:13 +0200

    Fixed htmlFor in forms

--- admin/src/cmps/questions update/admin-updates.jsx
+++ admin/src/cmps/questions update/admin-updates.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import { click } from "../../services/firebase.db";
 
+const handleSubmit = event => {
+    event.preventDefault();
+    console.log('form submitted ✅');
+  }
 
 export const Authenticated = (props) => {
     return <div>
         <div className="auth-title">Hello Admin 👋 </div>
 
-        <form >
-            <label className="underlined" for="questions">Choose a question number : </label>
+        <form onSubmit={handleSubmit} >
+            <label className="underlined" htmlFor="questions" >Choose a question number : </label>
             <select id="questions">
                 <option >Select a question</option>
                 <option value="1">1</option>
@@ -19,11 +23,11 @@ export const Authenticated = (props) => {
                 <option value="7">7</option>
                 <option value="8">8</option>
             </select>
-            <button className="validate-btn" onClick={click(1)}>Validate</button>
+            <button className="validate-btn" onClick={click(0)}>Validate</button>
         </form>
 
-        <label className="underlined" for="questions">Modify your question here : </label>
-        <input className="input-result" type="text" placeholder="Type your question..." />
+        <label className="underlined" htmlFor="input">Modify your question here : </label>
+        <input className="input-result" type="text" id="input" placeholder="Type your question..." />
 
         <button className="form-submit ">Submit</button>
     </div>
