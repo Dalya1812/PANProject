230ec90a8ed04d056a96473cbd6a97f3c4ba5f63
Tanya Grudinkina <MY_NAME@example.comtgr14345@gmail.com>
2023-02-28 21:53:52 +0200

    survey add, remove, update options added

--- admin/src/cmps/questions update/admin-updates.jsx
+++ admin/src/cmps/questions update/admin-updates.jsx
@@ -1,119 +1,192 @@
 import React, { useEffect, useState } from "react"
-import { numbers, getAllDb } from "../../services/firebase.db"
+// import { numbers, getAllDb } from "../../services/firebase.db"
+import { getAllDb, onSaveSurvey, removeFromSurvey } from "../../services/firebase.db"
+import { utilService } from '../../services/util.service'
+
+
+// const Input = ({ idx, obj, onInputChange }) => {
+//   return <input
+//     className="input-result"
+//     type="text"
+//     id="input-a"
+//     name={idx}
+//     placeholder="Type your response..."
+//     key={obj.answer}
+//     value={obj.answer}
+//     onChange={onInputChange}
+//   />
+// }
+
 
 export const Authenticated = () => {
 
-  const [selectedNumber, setSelectedNumber] = useState(0);
-  const [selectedQuestion, setSelectedQuestion] = useState("")
-  const [selectedAnswers, setSelectedAnswers] = useState("")
-  const [data, setData] = useState({})
+  const [selectedNumber, setSelectedNumber] = useState(false)
+  const [selectedQuestion, setSelectedQuestion] = useState('')
+  const [selectedAnswers, setSelectedAnswers] = useState([])
+  const [data, setData] = useState([])
 
   useEffect(() => {
     loadDb()
   }, [])
 
   const loadDb = async () => {
-    const data = await getAllDb()
-    setData(data)
-    console.table("databaseAdmin", data)
+    const dbData = await getAllDb()
+    const { surveyQuestions } = dbData
+    setData(surveyQuestions)
   }
 
-  const onSelectChange = (event) => {
-    const { value } = event.target
+  const onSelectChange = (ev) => {
+    const { value } = ev.target
     setSelectedNumber(value)
   }
 
-  const onInputChange = (event) => {
-    setSelectedQuestion(event.target.value)
-    setSelectedAnswers(event.target.value)
+  const onValidate = (ev, selectedNumber) => {
+    ev.preventDefault()
+    setSelectedQuestion(data[selectedNumber].question)
+    setSelectedAnswers(data[selectedNumber].answers)
   }
 
-  const onValidate = () => {
-    const question = data[selectedNumber].question
-    const answers = data[selectedNumber].answers
-    setSelectedQuestion(question)
-    setSelectedAnswers(answers)
-  };
+  const onInputChange = (ev) => {
+    const { id, name, value } = ev.target
+    let selectedAnswersCopy = JSON.parse(JSON.stringify(selectedAnswers))
+    if (id === 'input-q') {
+      setSelectedQuestion(value)
+    } else {
+      id === 'input-a' ?
+        selectedAnswersCopy[name].answer = value :
+        selectedAnswersCopy[name].points = +value
+
+      setSelectedAnswers(selectedAnswersCopy)
+    }
+  }
+
+  const onRemove = (ev, selectedNumber) => {
+    ev.preventDefault()
+      console.log('selectedNumber', selectedNumber)
+      removeFromSurvey(selectedNumber)
+      setSelectedQuestion('')
+      setSelectedNumber(false)
+      loadDb()
+  }
+
+  const onAdd = (ev) => {
+    ev.preventDefault()
+    // console.log(ev)
+    let dataCopy = JSON.parse(JSON.stringify(data))
+    const answers = Array.from({ length: 5 }, (_, index) => ({ answer: `${index + 1} הוסף תשובה`, points: 0 }))
+
+    const newQuestion = {
+      answers: answers,
+      question: "הוסף שאלה"
+    }
+
+    dataCopy.push(newQuestion)
+    setData(dataCopy)
+    setSelectedNumber(dataCopy.length - 1)
+    setSelectedQuestion(dataCopy[dataCopy.length - 1].question)
+    setSelectedAnswers(dataCopy[dataCopy.length - 1].answers)
+  }
+
 
   const handleSubmit = (event) => {
-    event.preventDefault();
-    // console.log(selectedQuestion)
-    // console.log(selectedAnswers)
-  };
+    event.preventDefault()
+    let dataCopy = JSON.parse(JSON.stringify(data))
+    dataCopy[selectedNumber].answers = selectedAnswers
+    dataCopy[selectedNumber].question = selectedQuestion
+    setData(dataCopy)
+    onSaveSurvey(dataCopy[selectedNumber], selectedNumber)
+    loadDb()
+  }
 
+  console.log('data', data)
+
+
+  if (!data) return <div>Loading...</div>
   return (
     <div>
       <div className="auth-title">Hello Admin 👋</div>
 
-      <form onSubmit={handleSubmit}>
-        <label className="underlined" htmlFor="questions">
-          Choose a question number :
-        </label>
-        <select onChange={onSelectChange} id="questions">
-          <option>Select a question</option>
-          {numbers.map((number) => (
-            <option value={number - 1} key={number}>
-              {number}
-            </option>
-          ))}
-        </select>
-        <button
-          className="validate-btn"
-          onClick={() => onValidate(selectedNumber)}
-        >
-          Validate
-        </button>
-
-        <label className="underlined" htmlFor="input-q">
-          Modify your question here :
-        </label>
-        <input
-          className="input-result"
-          type="text"
-          id="input-q"
-          placeholder="Type your question..."
-          value={selectedQuestion}
-          onChange={onInputChange}
-        />
-        <br />
-
-        <label className="underlined" htmlFor="input-q">
-          Modify your answers here :
-        </label>
-        <br />
+      <div className="select-question">
+        <span>Add question or select question number to edit/delete </span>
+        {/* Add Question */}
+        <button onClick={(ev) => onAdd(ev)}>Add</button>
+        <div className="select">
+          {/* Select question from DB */}
+          <label htmlFor="questions">Choose a question number :</label>
+          <select onChange={onSelectChange} id="questions" className='select-option'>
+            <option>{selectedNumber ? `Question number: ${+selectedNumber + 1}` : 'Select a question'}</option>
+            {data.map((_, idx) => (
+              <option value={idx} key={utilService.makeId()}>{idx + 1}</option>
+            ))}
+          </select>
+        </div>
 
         {selectedNumber &&
-          data[selectedNumber].answers.map((obj) => (
-            <section key={obj.points}>
+          <div className="action">
+              {/* Set Question */}
+              <button onClick={(ev) => onValidate(ev, selectedNumber)}>Edit</button>
 
-              <label htmlFor="input-q" className="label-for-answers">Answer:</label>
+              {/* Delete Question */}
+              <button onClick={(ev) => onRemove(ev, selectedNumber)}>Delete</button>
+          </div>}
+
+      </div>
+
+      {selectedQuestion &&
+        <form onSubmit={handleSubmit} className="form-data">
+
+          {/* Update question */}
+          <label htmlFor="input-q">Modify your question here :</label>
+          <br />
+
+          <input
+            className="input-result"
+            type="text"
+            id="input-q"
+            placeholder="Type your question..."
+            value={selectedQuestion}
+            onChange={onInputChange}
+          />
+          <br />
+
+          <label className="underlined" htmlFor="input-q">Modify your answers here :</label>
+          <br />
+
+          {data[selectedNumber]?.answers.map((obj, idx) => (
+            <section key={idx}>
+
+              <label htmlFor="input-a" className="label-for-answers">Answer:</label>
               <input
-                className="input-result"
+                className="input-result-answer"
                 type="text"
-                id="input-q"
+                id="input-a"
+                name={idx}
                 placeholder="Type your response..."
                 key={obj.answer}
-                value={obj.answer}
+                defaultValue={obj.answer}
                 onChange={onInputChange}
               />
 
-              <label htmlFor="input-a" className="label-for-answers">Points:</label>
+              <label htmlFor="input-p" className="label-for-points">Points:</label>
               <input
+                min="0"
+                max="4"
                 className="input-result-points"
-                type="text"
-                id="input-a"
+                type="number"
+                id="input-p"
+                name={idx}
                 placeholder="Type your response..."
                 key={obj.points}
-                value={obj.points}
-                onChange={onInputChange}
+                defaultValue={obj.points}
+                onInput={onInputChange}
               />
             </section>
           ))}
 
-        <button className="form-submit" type="submit">
-          Submit
-        </button>
-      </form>
+
+          <button className="form-submit" type="submit">Save</button>
+        </form>}
+
     </div>
   );
 };
--- admin/src/services/firebase.db.js
+++ admin/src/services/firebase.db.js
@@ -1,33 +1,17 @@
 import firebase from "firebase/compat/app"
 import firebaseConfig from '../firebase.config'
-import { getDatabase, ref, child, get, push, update, onValue } from "firebase/database"
+import { getDatabase, ref, child, get, push, update, onValue, remove } from "firebase/database"
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig)
 }
 
-
-export async function getAllDb() {
+export const getAllDb = async () => {
     try {
         const dbRef = ref(getDatabase())
-        const allDb = await get(dbRef)
-        if (!allDb.exists()) {
-            alert("Please choose a question number !");
-        }
-        return allDb.val()
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-export const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-
-export async function getQuestionByNumber(number) {
-    try {
-        const dbRef = ref(getDatabase())
-        const snapshot = await get(child(dbRef, `${number}/question`))
+        const snapshot = await get(dbRef)
         if (!snapshot.exists()) {
-            alert("Please choose a question number !");
+            console.log("Can't get data from DB! !")
         }
         return snapshot.val()
     } catch (error) {
@@ -35,36 +19,48 @@ export async function getQuestionByNumber(number) {
     }
 }
 
-export async function getAnswersByNumber(number) {
-    for (var i = 0; i < 5; i++) {
-        try {
-            const dbRef = ref(getDatabase())
-            const snapshot = await get(child(dbRef, `${number}/answers/${i}/answer`))
-            if (!snapshot.exists()) {
-                alert("Please choose a question number !")
-            }
-            return snapshot.val();
-        } catch (error) {
-            console.error(error)
+export const getSurveyQuestion = async (number) => {
+    try {
+        const dbRef = ref(getDatabase())
+        const questionRef = child(dbRef, `surveyQuestions/${number}/question`)
+        const snapshot = await get(child(dbRef, questionRef))
+        if (!snapshot.exists()) {
+            console.log("Can't get survey question!")
         }
-
+        return snapshot.val()
+    } catch (error) {
+        console.error(error);
     }
 }
 
-export function onUpdateQuestion(number, question) {
+export const onSaveSurvey = async (surveyQuestion, selectedNumber) => {
     try {
         const dbRef = ref(getDatabase())
-        const postData = {
-            question: question,
-        }
+        const surveyRef = child(dbRef, `surveyQuestions`)
 
-        const newPostKey = push(child(dbRef, `${number}/${question}`)).key
+        console.log('surveyQuestion', surveyQuestion)
+        const postData = surveyQuestion.id ?
+            { ...surveyQuestion } :
+            { ...surveyQuestion, id: push(surveyRef).key }
+
         const updates = {};
-        updates[newPostKey] = postData;
-        return update(dbRef, updates);
-        alert("Question was updated successfully !")
+        updates[selectedNumber] = postData
+        console.log('updates', updates)
+        await update(surveyRef, updates)
+
+
     } catch (error) {
         console.error(error)
     }
+}
 
+export const removeFromSurvey = async (id) => {
+    try {
+        const dbRef = ref(getDatabase())
+        const surveyRef = child(dbRef, `surveyQuestions/${id}`);
+        await remove(surveyRef)
+        console.log("Question was removed successfully from DB!")
+    } catch (err) {
+        console.log('Could not remove error from DB', err)
+    }
 }
